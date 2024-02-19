d974ec260090a645f5cf744dea70453a86b31abc
JorgeWS <jorgews.dev@gmail.com>
2024-02-19 14:07:11 -0300

    feat: 🎸 Cofigure middleware and and status_code to response of endpoints

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,14 +3,14 @@ export const routes = [
         method: 'GET',
         path: '/tasks',
         handler: (req, res) => {
-            return res.end('GET Tasks')
+            return res.writeHead(200).end('GET Tasks')
         }
     },
     {
         method: 'POST',
         path: '/tasks',
         handler: (req, res) => {
-            return res.end('POST Tasks')
+            return res.writeHead(200).end('POST Tasks')
         }
     }
 ]
--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 import { createServer } from 'node:http';
+import { json } from './middleware/json.js';
 import { routes } from './routes/route.js';
 
-const server = createServer((req, res) => {
+const server = createServer(async (req, res) => {
 
     const { method, url } = req;
+
+    await json(req, res);
 
     const route = routes.find(r => r.method === method && r.path === url)
 
@@ -12,8 +15,7 @@ const server = createServer((req, res) => {
     if (route) {
         return route.handler(req, res);
     }
-
-    res.end('hello world')
+    res.writeHead(400).end(JSON.stringify({ success: false }))
 });
 
 server.listen(3333)
